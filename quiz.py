d3efeacadd067b41bf1bf69a1b5d3bf758e61d6d
mbuguapurity <mbuguapurity1@gmail.com>
2019-05-07 19:17:53 +0300

    add files

---
+++ quiz.py
@@ -0,0 +1,74 @@
+def tens(i):
+	h = range(0,10)
+	for x in i:
+		h[x]= x**10
+	print(h)
+
+def comps(g):
+	x = [2,4,6,8,10,12]
+	y =[]
+	for x in g:
+		y = x**2
+		print (y) 
+
+
+
+
+
+def sorted(x):
+	x = [[9,2,4,7,3],[5,2,6,8,0]]
+	h =[]
+	for m in x:
+		a=b
+		h.append(z)
+
+	x = [9,2,4,7,3,5,2,6,8,0]
+	x.sort()
+	x
+
+def sumoflist(n):
+	a=[]
+	for x in range(1,n):
+		a.append(x)
+		b=sum(a)
+	return b
+
+
+def largest(i):
+	n = []
+	for x in lists:
+		if x%i==0:
+			n.append(x)
+			print(n)
+			print(max(n))
+
+
+
+def smallest(i):
+	m = []
+	for x in lists:
+		if x%i==0:
+			m.append(x)
+			print(m)
+			print(min(m))
+
+
+
+def my_modulus(l):
+	g=dict()
+	for x in l:
+		g[x] = x**3
+	print(g)
+
+h = range(10,20)
+f= dict()
+for x in h:
+	f[x]= x**3
+print(f)
+
+def person(name,balance):
+	print( "Hi {} ,your loan balance is {}".format(student["name"],balance))
+
+
+
+		
